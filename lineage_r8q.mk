2a40a8ad4f2ad8db13725a92376f4fdc0f0c3ec2
joaquimiguel <joaquimmiguel.vasco@gmail.com>
2026-05-05 01:26:29 -0300

    Add some build flags.

--- lineage_r8q.mk
+++ lineage_r8q.mk
@@ -29,6 +29,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Build flags
+BYPASS_CHARGE_SUPPORTED := true
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+TARGET_CUSTOM_UDFPS := true
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_r8q
 PRODUCT_DEVICE := r8q
